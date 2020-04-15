8e55af24e8f2552209e4f62866dcb3ec8748700d
Naseem <Naseem-ali@hotmail.com>
2020-04-16 00:43:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app_port = process.env.PORT || 3001
+const express  = require('express')
+const app_port = process.env.PORT || 3000
 const app = express()
+
 app.get('/', (req, res) => {
-    res.send('Hello Naseem')
+    res.send('hello world')
 })
+
 app.listen(app_port)
 console.log(`app is running. port: ${app_port}`)
 console.log(`http://127.0.0.1:${app_port}/`)
