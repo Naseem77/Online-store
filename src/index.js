075f917f3bd5078a00d0c2898edd82341edc9df1
Shay Manasherov <shay28xz@gmail.com>
2020-04-16 14:29:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express  = require("express")
 const app_port = process.env.PORT || 3000
 const app = express()
 
-app.get("/", (req, res) => {    res.send("hello world welcome")
+app.get("/", (req, res) => {    res.send("hello world welcome to")
 })
 
 app.listen(app_port)
