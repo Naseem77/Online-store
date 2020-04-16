055621d3ed7577c56c1e0c6ccb125cb204c3da09
Naseem <Naseem-ali@hotmail.com>
2020-04-16 15:42:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app_port = process.env.PORT || 3000
 const app = express()
 
 app.get('/', (req, res) => {
-	res.send('hello world')
+	res.send('Welcome to our online-store app')
 })
 
 app.listen(app_port)
